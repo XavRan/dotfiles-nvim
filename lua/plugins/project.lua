15d5766c9d2cc5e05551729562fd8fff439293f0
XavRan <XavRan@users.noreply.github.com>
2026-07-30 05:02:53 +0800

    fix: project.nvim fork uses require('project') not require('project_nvim')

--- lua/plugins/project.lua
+++ lua/plugins/project.lua
@@ -3,7 +3,7 @@ return {
   "DrKJeff16/project.nvim",
   event = "VeryLazy",
   config = function()
-    require("project_nvim").setup({})
+    require("project").setup({})
     require("telescope").load_extension("projects")
     vim.keymap.set("n", "<leader>sp", ":Telescope projects<CR>", { desc = "[S]earch [P]rojects" })
   end,
